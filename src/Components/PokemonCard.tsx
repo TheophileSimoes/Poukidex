a94222007fb51520428d5a907fb29e67149c2628
TheophileSimoes <theophile.sdn@gmail.com>
2024-10-22 16:51:36 +0200

    Ajout de Mew

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,7 +1,23 @@
 function PokemonCard () {
-    return <><figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-    </figure><figcaption>Bulbasaur</figcaption></>
-    }
+
+    let pokemon = pokemonList[0];
+   
     
+    return <><figure>
+       {pokemon.imgSrc ? (<img src = {pokemon.imgSrc} alt ={pokemon.image}/>) : (<p>???</p>) } 
+    </figure><figcaption>{ pokemon.name  ? (<p>{pokemon.name}</p>) : (<p>???</p> )}</figcaption></>
+    
+    
+    }
+    const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
+
     export default PokemonCard;
